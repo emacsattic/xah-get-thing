edf602fcf95d4e3f8c41f8cf29812fc2b4657aa8
Xah Lee <xah@xahlee.org>
2016-10-18 06:01:38 -0700

    rewrite. new functions, so compatible with thing-at-point.

--- xah-get-thing.el
+++ xah-get-thing.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2011-2015 by Xah Lee
 
 ;; Author: Xah Lee ( http://xahlee.org/ )
-;; Version: 1.0.1
+;; Version: 2.0.0
 ;; Created: 22 May 2015
 ;; Keywords: extensions, lisp, tools
 ;; URL: http://ergoemacs.org/emacs/elisp_get-selection-or-unit.html
@@ -18,12 +18,13 @@
 
 ;; The functions are:
 
-;; xah-get-thing-at-cursor
-;; xah-get-thing-or-selection
+;; xah-get-bounds-of-thing
+;; xah-get-bounds-of-thing-or-region
+;; xah-get-thing-at-point
+;; xah-get-thing-at-cursor (deprecated)
+;; xah-get-thing-or-selection (deprecated)
 
 ;; They get “thing” independent of syntax table, so you always get same thing regardless what's current major mode.
-
-;; xah-get-thing-or-selection get text selection when there's active region.
 
 ;; Call describe-function on them to read the inline doc.
 ;; Home page: http://ergoemacs.org/emacs/elisp_get-selection-or-unit.html
@@ -44,13 +45,14 @@
 
 ;;; Code:
 
-(defun xah-get-thing-at-cursor (φunit)
-  "Return the string and boundary of ΦUNIT under cursor.
-Returns vector [text a b], where text is the string and a b are its boundary.
+(defun xah-get-bounds-of-thing (*unit )
+  "Return the boundary of *UNIT under cursor.
 
-ΦUNIT can be:
+Return a cons cell (START . END).
 
-• 'word — sequence of hyphen, 0 to 9, A to Z, a to z, and other chars such as àáâãäå.
+*UNIT can be:
+
+• 'word — sequence of 0 to 9, A to Z, a to z, and hyphen.
 
 • 'glyphs — sequence of visible glyphs. Useful for file name, URL, …, anything doesn't have white spaces in it.
 
@@ -66,45 +68,45 @@ Returns vector [text a b], where text is the string and a b are its boundary.
 
 • a vector [beginRegex endRegex] — The elements are regex strings used to determine the beginning/end of boundary chars. They are passed to `skip-chars-backward' and `skip-chars-forward'. For example, if you want paren as delimiter, use [\"^(\" \"^)\"]
 
-Example usage:
- (setq bds (xah-get-thing-at-cursor 'line))
- (setq inputstr (elt bds 0) p1 (elt bds 1) p2 (elt bds 2)  )
+This function is similar to `bounds-of-thing-at-point'.
+The main difference are:
 
-This function is similar to `thing-at-point' and `bounds-of-thing-at-point'.
-The main differences are:
-
-• This function returns the text and the 2 boundaries as a vector in one shot.
+• This function's behavior does not depend on syntax table. e.g. for *units 「'word」, 「'block」, etc.
 
 • 'line always returns the line without end of line character, avoiding inconsistency when the line is at end of buffer.
 
-• This function's behavior does not depend on syntax table. e.g. for units 「'word」, 「'block」, etc."
+• Support certain “thing” such as 'glyphs that's a sequence of chars. Useful as file path or url in html links, but do not know which before hand.
+
+• Some “thing” such 'url and 'filepath considers strings that at USUALLY used for such. The algorithm that determines this is different from thing-at-point.
+
+Version 2016-10-18"
   (let (p1 p2)
     (save-excursion
       (cond
-       ( (eq φunit 'word)
+       ( (eq *unit 'word)
          (let ((wordcharset "-A-Za-z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ"))
            (skip-chars-backward wordcharset)
            (setq p1 (point))
            (skip-chars-forward wordcharset)
            (setq p2 (point))))
 
-       ( (eq φunit 'glyphs)
+       ( (eq *unit 'glyphs)
          (progn
            (skip-chars-backward "[:graph:]")
            (setq p1 (point))
            (skip-chars-forward "[:graph:]")
            (setq p2 (point))))
 
-       ((eq φunit 'buffer)
+       ((eq *unit 'buffer)
         (progn
           (setq p1 (point-min))
           (setq p2 (point-max))))
 
-       ((eq φunit 'line)
+       ((eq *unit 'line)
         (progn
           (setq p1 (line-beginning-position))
           (setq p2 (line-end-position))))
-       ((eq φunit 'block)
+       ((eq *unit 'block)
         (progn
           (if (re-search-backward "\n[ \t]*\n" nil "move")
               (progn (re-search-forward "\n[ \t]*\n")
@@ -115,7 +117,7 @@ The main differences are:
                      (setq p2 (point)))
             (setq p2 (point)))))
 
-       ((eq φunit 'filepath)
+       ((eq *unit 'filepath)
         (let (p0)
           (setq p0 (point))
           ;; chars that are likely to be delimiters of full path, e.g. space, tabs, brakets.
@@ -125,47 +127,61 @@ The main differences are:
           (skip-chars-forward "^  \"\t\n'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\'")
           (setq p2 (point))))
 
-       ((eq φunit 'url)
+       ((eq *unit 'url)
         (let (p0
-              ;; (ξdelimitors "^ \t\n,()[]{}<>〔〕“”\"`'!$^*|\;")
-              (ξdelimitors "!\"#$%&'*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"))
+              ;; (-delimitors "^ \t\n,()[]{}<>〔〕“”\"`'!$^*|\;")
+              (-delimitors "!\"#$%&'*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"))
           (setq p0 (point))
-          (skip-chars-backward ξdelimitors) ;"^ \t\n,([{<>〔“\""
+          (skip-chars-backward -delimitors) ;"^ \t\n,([{<>〔“\""
           (setq p1 (point))
           (goto-char p0)
-          (skip-chars-forward ξdelimitors) ;"^ \t\n,)]}<>〕\"”"
+          (skip-chars-forward -delimitors) ;"^ \t\n,)]}<>〕\"”"
           (setq p2 (point))))
 
-       ((vectorp φunit)
+       ((vectorp *unit)
         (let (p0)
           (setq p0 (point))
-          (skip-chars-backward (elt φunit 0))
+          (skip-chars-backward (elt *unit 0))
           (setq p1 (point))
           (goto-char p0)
-          (skip-chars-forward (elt φunit 1))
+          (skip-chars-forward (elt *unit 1))
           (setq p2 (point))))))
 
-    (vector (buffer-substring-no-properties p1 p2) p1 p2 )))
+    (cons p1 p2 )))
 
-(defun xah-get-thing-or-selection (φunit)
-  "Return the string and boundary of text selection or ΦUNIT under cursor.
-
-Returns a vector [text a b], where text is the string and a b are its boundary.
-
-If `use-region-p' is true, then text is the region.  Else, it depends on the ΦUNIT, which can be 'word 'line 'block 'buffer 'filepath 'url …. See `xah-get-thing-at-cursor' for detail.
-
-Example usage:
-(progn
-    (setq boundary (xah-get-thing-or-selection 'block))
-    (setq
-     input-str (elt boundary 0)
-     p1 (elt boundary 1)
-     p2 (elt boundary 2)))"
-  (interactive)
+(defun xah-get-bounds-of-thing-or-region (*unit)
+  "Same as `xah-get-bounds-of-thing', except when (use-region-p) is t, return the region boundary instead.
+Version 2016-10-18"
   (if (use-region-p)
-      (let ((p1 (region-beginning)) (p2 (region-end)))
-        (vector (buffer-substring-no-properties p1 p2) p1 p2 ))
-    (xah-get-thing-at-cursor φunit)))
+      (car (region-beginning) (region-end))
+    (xah-get-bounds-of-thing *unit)))
+
+(defun xah-get-thing-at-point (*unit)
+  "Same as `xah-get-bounds-of-thing', but return the string.
+Version 2016-10-18T02:31:36-07:00"
+  (let ( (-bds (xah-get-bounds-of-thing *unit)) )
+    (buffer-substring-no-properties (car -bds) (cdr -bds))))
+
+(defun xah-get-thing-at-cursor (*unit)
+  "Same as `xah-get-bounds-of-thing', except this returns a vector [text a b], where text is the string and a b are its boundary.
+
+Version 2016-10-18T00:23:52-07:00"
+  (let* (
+         (-bds (xah-get-bounds-of-thing *unit))
+         (-p1 (car -bds)) (-p2 (cdr -bds)))
+    (vector (buffer-substring-no-properties -p1 -p2) -p1 -p2 )))
+
+(make-obsolete 'xah-get-thing-at-cursor 'xah-get-thing-at-point "2016-10-18")
+
+(defun xah-get-thing-or-selection (*unit)
+  "Same as `xah-get-bounds-of-thing-or-region', except returns a vector [text a b], where text is the string and a b are its boundary."
+  (interactive)
+  (let* (
+         (-bds (xah-get-bounds-of-thing-or-region *unit))
+         (-p1 (car -bds)) (-p2 (cdr -bds)))
+    (vector (buffer-substring-no-properties -p1 -p2) -p1 -p2 )))
+
+(make-obsolete 'xah-get-thing-or-selection 'xah-get-bounds-of-thing-or-region "2016-10-18")
 
 (provide 'xah-get-thing)
 
